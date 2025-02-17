528fb50dbf1a628749c03ea99d90be0ddc932086
Miraidon128 <DLaprasYT@users.noreply.github.com>
2025-02-17 17:52:10 -0500

    fixed bugs

---
+++ src/precomp/__init__.py
@@ -0,0 +1,5 @@
+from precomp import bottomlessPit
+
+def bottomlessPit(vmf, max_height):
+    """Función en precomp que llama a bottomlessPit.make_bottomless_pit."""
+    bottomlessPit.make_bottomless_pit(vmf, max_height)
