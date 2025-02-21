5231031847de89197eb68622c8389635fa238d69
Miraidon128 <DLaprasYT@users.noreply.github.com>
2025-02-21 15:34:55 -0500

    Update vbsp.py

--- src/vbsp.py
+++ src/vbsp.py
@@ -971,8 +971,7 @@ def change_brush(vmf: VMF) -> None:
 
     if make_bottomless:
         LOGGER.info('Creating Bottomless Pits...')
-        from precomp import bottomlessPit
-        bottomlessPit(vmf, max_height)
+        bottomlessPit_make(vmf, max_height)
         LOGGER.info('Done!')
     if make_goo_mist:
         LOGGER.info('Adding Goo Mist...')
